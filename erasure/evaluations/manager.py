ba16915289f3c23a6347ceccce7a6fc9880b49ab
realgabriele <realgabriele@gmail.com>
2024-10-09 09:59:54 +0200

    evaluation changes
    created abstract class Measure(Configurable)
    created abstract class UnlearnRunner(Measure)
    moved compute_accuracy as a utility

--- erasure/evaluations/manager.py
+++ erasure/evaluations/manager.py
@@ -1,12 +1,17 @@
+import sys
+
 from erasure.core.base import Configurable
 from erasure.core.factory_base import get_instance_kvargs
+from erasure.evaluations.evaluation import Evaluation
+from erasure.evaluations.running import UnlearnRunner
 from erasure.utils.config.global_ctx import Global
 from erasure.core.unlearner import Unlearner
-import sys
+from erasure.utils.config.local_ctx import Local
+
 
 class Evaluator(Configurable):
 
-    def __init__(self, global_ctx: Global, local_ctx):
+    def __init__(self, global_ctx: Global, local_ctx: Local):
         super().__init__(global_ctx, local_ctx)
 
         self.__init_measures__()
@@ -19,17 +24,16 @@ class Evaluator(Configurable):
         return e
 
     def __init_measures__(self):
-        self.measures= [get_instance_kvargs(measure['class'],measure['parameters']) for measure in self.params['measures']]
-        
-class Evaluation():
-    def __init__(self,unlearner: Unlearner):
-        self.data_info = {}
-        self.unlearned_model = None
-        self.unlearner = unlearner
-        self.forget_set = unlearner.dataset.partitions['forget set']
-        self.data_info['unlearner'] = unlearner.__class__.__name__
-        self.data_info['dataset'] = unlearner.dataset.name
-        self.data_info['parameters'] = unlearner.params
+        self.measures = []
+        for measure in self.params['measures']:
+            current = Local(measure)
+            self.measures.append( self.global_ctx.factory.get_object(current) )
 
-    def add_value(self, key, value):
-        self.data_info[key] = value
+        # the first metric has to be one that calls the unlearn() method of the unlearner
+        if not isinstance(self.measures[0], UnlearnRunner):
+            config = {"class": "erasure.evaluations.running.UnlearnRunner", "parameters":{}}
+            current = Local(config)
+            self.measures.insert(0, self.global_ctx.factory.get_object(current))
+
+        assert isinstance(self.measures[0], UnlearnRunner)
+
---
+++ erasure/evaluations/running.py
@@ -0,0 +1,27 @@
+import time
+
+from erasure.core.measure import Measure
+from erasure.evaluations.manager import Evaluation
+
+
+class UnlearnRunner(Measure):
+    """ Generic measure class that calls the unlearn() method """
+
+    def process(self, e: Evaluation):
+        e.unlearned_model = e.unlearner.unlearn()
+
+        return e
+
+
+class RunTime(UnlearnRunner):
+    def process(self, e: Evaluation):
+        if not e.unlearned_model:
+            start_time = time.time()
+
+            e.unlearned_model = e.unlearner.unlearn()
+            metric_value = time.time() - start_time
+
+        e.add_value('RunTime', metric_value)
+
+        return e
+
