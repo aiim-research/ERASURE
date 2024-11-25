54521d51af7b1a36d5d0c10ce1eadcd94b5b5297
dangeloandrea14 <dangeloandrea14@gmail.com>
2024-11-25 17:42:41 +0100

    added UCI Repo data source. Improved TV DataSource.

--- erasure/data/data_sources/TVDataSource.py
+++ erasure/data/data_sources/TVDataSource.py
@@ -1,6 +1,7 @@
 from .datasource import DataSource
 from erasure.data.datasets.Dataset import Dataset 
 from torch.utils.data import ConcatDataset
+import inspect 
 
 class TVDataSource(DataSource):
     def __init__(self, path):
@@ -20,15 +21,25 @@ class TVDataSource(DataSource):
             m = getattr(m, part)
         
         dataset_class = getattr(m, parts[-1])
+
+        print(f"dataset_class is {dataset_class}")
+
+        params = inspect.signature(dataset_class.__init__).parameters
+
     
-        try:
+        #try:
+        if 'train' in params:
             train = dataset_class(train=True, root='resources/data', download=True, transform=lib.transforms.ToTensor())
             test = dataset_class(train=False, root='resources/data', download=True, transform=lib.transforms.ToTensor())
-            concat =  ConcatDataset([train, test])
-        except:
-            raise ValueError(f"Dataset {self.dataset_name} is not available.")
+        elif 'split' in params:
+            train = dataset_class(split='train', root='resources/data', download=True, transform=lib.transforms.ToTensor())
+            test = dataset_class(split='test', root='resources/data', download=True, transform=lib.transforms.ToTensor())
+        else:
+            raise ValueError("Unknown dataset parameters.")
+        #except:
+            #raise ValueError(f"Dataset {dataset_class} is not available.")
 
-
+        concat =  ConcatDataset([train, test])
         dataset = Dataset(concat)
 
         return dataset
