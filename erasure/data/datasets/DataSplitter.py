c221cc2f0bb2be0089c1b7dc837f3bd814c47605
dangeloandrea14 <dangeloandrea14@gmail.com>
2025-01-05 12:37:09 +0100

    datasplitter percentage now shuffles + resnet for celeba

--- erasure/data/datasets/DataSplitter.py
+++ erasure/data/datasets/DataSplitter.py
@@ -5,6 +5,7 @@ from erasure.core.base import Configurable
 from .Dataset import DatasetWrapper
 from tqdm import tqdm
 from torch.utils.data import DataLoader
+import torch
 
 class DataSplitter(ABC):
     def __init__(self, ref_data,parts_names):
@@ -20,9 +21,10 @@ class DataSplitter(ABC):
 
     
 class DataSplitterPercentage(DataSplitter):
-    def __init__(self, percentage, parts_names, ref_data = 'all'):
+    def __init__(self, percentage, parts_names, ref_data = 'all', shuffle=True):
         super().__init__(ref_data,parts_names) 
         self.percentage = percentage
+        self.shuffle = shuffle
 
     def split_data(self,partitions):
         ref_data = partitions[self.ref_data] if self.ref_data == 'all' else self.source.get_extended_wrapper(Subset(partitions['all'].data, partitions[self.ref_data]))
@@ -30,7 +32,9 @@ class DataSplitterPercentage(DataSplitter):
         total_size = len(ref_data.data)
         split_point = int(total_size * self.percentage)
 
-        indices = list(range(total_size))
+        indices = list(range(total_size)) if not self.shuffle else torch.randperm(total_size)
+
+        print(indices)
 
         split_indices_1 = indices[:split_point]
         split_indices_2 = indices[split_point:]
@@ -39,6 +43,27 @@ class DataSplitterPercentage(DataSplitter):
         partitions[self.parts_names[1]] = split_indices_2
 
         return partitions
+    
+    
+class DataSplitterConcat(DataSplitter):
+    def __init__(self, concat_splits, parts_names, ref_data = 'all'):
+        super().__init__(ref_data,parts_names) 
+        self.concat_splits = concat_splits
+
+    def split_data(self, partitions):
+        unified_indices = []
+
+        for split in self.concat_splits:
+            if split in partitions:
+                unified_indices.extend(partitions[split])
+            else:
+                raise KeyError(f"Partition '{split}' not found in partitions.")
+
+        partitions[self.parts_names[0]] = unified_indices
+
+
+        return partitions
+
 
 class DataSplitterClass(DataSplitter):
     def __init__(self, label, parts_names, ref_data = 'all'):
