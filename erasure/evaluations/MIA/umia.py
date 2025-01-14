30b8ee6be05edec7a3bcdbd1e416681a29435222
realgabriele <realgabriele@gmail.com>
2025-01-14 19:20:26 +0100

    U-MIA
    fixed and working

--- erasure/evaluations/MIA/umia.py
+++ erasure/evaluations/MIA/umia.py
@@ -29,6 +29,7 @@ class Attack(Measure):
         self.forget_part = 'forget'
         self.test_part = 'test'
 
+        self.params["loss_fn"]["parameters"]["reduction"] = "none"
         self.loss_fn = get_instance_config(self.params['loss_fn'])
 
     def check_configuration(self):
@@ -47,7 +48,7 @@ class Attack(Measure):
 
         # generate dataset from sampling the target model
         self.info("Creating attack dataset")
-        attack_dataset = self.__create_attack_dataset(target_model)
+        attack_dataset = self.create_attack_dataset(target_model)
 
         # build a binary classifier
         self.info("Creating attack model")
@@ -62,32 +63,28 @@ class Attack(Measure):
             umia_accuracy = compute_accuracy(test_loader, attack_model.model)
 
         else:
-            # Hardcoded Logistic Regression
-            train_loader, _ = attack_dataset.get_loader_for("train")
-            X_train, y_train = train_loader.dataset[:]
-
+            # sklearn Logistic Regression
+            attack_loader, _ = attack_dataset.get_loader_for("all")
+            X, y = attack_loader.dataset[:]
             attack_model = sklearn.linear_model.LogisticRegression()
-            attack_model.fit(X_train, y_train)
+            cv = sklearn.model_selection.StratifiedShuffleSplit(n_splits=5, test_size=0.8)
+            accuracies = sklearn.model_selection.cross_val_score(
+                attack_model, X, y, cv=cv, scoring="accuracy")
 
-            # Compute accuracy
-            test_loader, _ = attack_dataset.get_loader_for("test")
-            X_test, y_test = test_loader.dataset[:]
-            umia_accuracy = sklearn.metrics.accuracy_score(y_test, attack_model.predict(X_test))
+            umia_accuracy = accuracies.mean()
 
         self.info(f"UMIA accuracy: {umia_accuracy}")
         e.add_value("UMIA", umia_accuracy)
 
         return e
 
-
-
-    def __create_attack_dataset(self, target_model):
+    def create_attack_dataset(self, target_model):
         """ Create the attack dataset from the target model"""
         attack_samples = []
         attack_labels = []
 
         # Attack Dataset creation
-        samples, labels = self.__get_attack_samples(target_model)
+        samples, labels = self.get_attack_samples(target_model)
         attack_samples.append(samples)
         attack_labels.append(labels)
 
@@ -101,7 +98,7 @@ class Attack(Measure):
         attack_labels = attack_labels[perm_idxs]
 
         # create Datasets
-        n_classes = 1
+        n_classes = 2
         attack_dataset = torch.utils.data.TensorDataset(attack_samples, attack_labels)
         attack_dataset.n_classes = n_classes
 
@@ -112,49 +109,47 @@ class Attack(Measure):
 
         return attack_datamanager
 
-
-    def __get_attack_samples(self, model):
+    def get_attack_samples(self, model):
         """ From the unlearned model, generate the attack samples """
 
         forget_loader, _ = model.dataset.get_loader_for(self.forget_part)
         test_loader, _ = model.dataset.get_loader_for(self.test_part)
 
+        forget_samples, forget_labels = self.generate_samples(model, forget_loader, 1)
+        test_samples, test_labels = self.generate_samples(model, test_loader, 0)
+
         # we need the same number of samples from each partition
-        samples_size = min(len(forget_loader.dataset), len(test_loader.dataset))
+        samples_size = min(len(forget_samples), len(test_samples))
 
-        attack_samples = []
-        attack_labels = []
+        forget_samples = forget_samples[:samples_size]
+        forget_labels = forget_labels[:samples_size]
+        test_samples = test_samples[:samples_size]
+        test_labels = test_labels[:samples_size]
 
-        forget_samples, forget_labels = self.__generate_samples(model, forget_loader, 1)
-        attack_samples.append(forget_samples[:samples_size])
-        attack_labels.append(forget_labels[:samples_size])
+        return torch.cat([forget_samples, test_samples]), torch.cat([forget_labels, test_labels])
 
-        test_samples, test_labels = self.__generate_samples(model, test_loader, 0)
-        attack_samples.append(test_samples[:samples_size])
-        attack_labels.append(test_labels[:samples_size])
-
-        return torch.cat(attack_samples), torch.cat(attack_labels)
-
-    def __generate_samples(self, model, loader, label_value):
+    def generate_samples(self, model, loader, label_value):
         attack_samples = []
         attack_labels = []
 
         with torch.no_grad():
             for X, labels in loader:
-                original_labels = labels.view(len(labels), -1)
                 X = X.to(model.device)
                 _, predictions = model.model(X)  # model prediction
 
-                losses = torch.tensor([self.loss_fn(predictions[i], labels[i]) for i in range(len(predictions))])
+                losses = self.loss_fn(predictions, labels)
+                # predictions = torch.nn.functional.softmax(predictions, dim=0)
 
-                predictions = predictions.to('cpu')
                 losses = losses.to('cpu')
+                predictions = predictions.to('cpu')
 
                 attack_samples.append(
                     losses.unsqueeze(1)
+                    # predictions
                 )
                 attack_labels.append(
                     torch.full([len(X)], label_value, dtype=torch.float32)   # 1: forgetting samples, 0: testing samples
+                    # torch.full([len(X)], label_value, dtype=torch.long)
                 )
 
         return torch.cat(attack_samples), torch.cat(attack_labels)
