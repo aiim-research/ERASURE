89954dea8016c0d8cb55b21168c7fb22991a4813
dangeloandrea14 <dangeloandrea14@gmail.com>
2025-07-01 18:09:03 +0200

    fix

--- erasure/data/preprocessing/reshape_y_z.py
+++ erasure/data/preprocessing/reshape_y_z.py
@@ -42,3 +42,34 @@ class reshape_y_z(Preprocess):
         super().check_configuration()
         self.local_config['parameters']['keep_as_y'] = self.local_config['parameters'].get('keep_as_y', [])
         self.local_config['parameters']['move_to_z'] = self.local_config['parameters'].get('move_to_z', [])
+
+
+
+
+class reshape_y_z_legacy(Preprocess):
+    def __init__(self, global_ctx: Global, local_ctx: Local):
+        super().__init__(global_ctx, local_ctx)
+        self.keep_as_y = self.local_config['parameters']['keep_as_y']
+        self.move_to_z = self.local_config['parameters']['move_to_z']
+
+
+    def process(self, X, y, z):
+
+        ##  input tensor is of shape
+        ##  ( [img], [downloaded_attributes], None)
+        ##  if keep_as_y is (0,0), we keep the first column of the first downloaded_attribute
+
+        true_y = y
+
+        for idx in self.keep_as_y:
+            true_y = true_y[idx]
+
+        z = y
+        for idx in self.move_to_z:
+            z = z[idx]
+
+        return X, true_y , z
+
+    def check_configuration(self):
+        super().check_configuration()
+        self.local_config['parameters']['keep_as_y'] = self.local_config['parameters'].get('keep_as_y',0)
