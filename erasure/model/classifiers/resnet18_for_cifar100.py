1f4d3e0af455d941209c5c79f34f12c78920b743
Claudio Savelli <claudiosav2000@gmail.com>
2025-08-12 18:57:30 +0200

    resnet50 added

--- erasure/model/classifiers/resnet18_for_cifar100.py
+++ erasure/model/classifiers/resnet18_for_cifar100.py
@@ -1,10 +1,10 @@
 import torch
 import torch.nn as nn
-from torchvision.models import resnet18, resnet50
+from torchvision.models import resnet18, resnet50, vgg16
 
-class CelebAResNet18(nn.Module):
-    def __init__(self, n_classes=2):
-        super(CelebAResNet18, self).__init__()
+class Cifar100ResNet18(nn.Module):
+    def __init__(self, n_classes=100):
+        super(Cifar100ResNet18, self).__init__()
         
         resnet = resnet18()
         
@@ -27,10 +27,11 @@ class CelebAResNet18(nn.Module):
         x = self.fc2(x)
         
         return intermediate_output, x
-    
-class CelebAResNet50(nn.Module):
-    def __init__(self, n_classes=2):
-        super(CelebAResNet50, self).__init__()
+
+
+class Cifar100ResNet50(nn.Module):
+    def __init__(self, n_classes=100):
+        super(Cifar100ResNet50, self).__init__()
         
         resnet = resnet50()
         
@@ -52,4 +53,26 @@ class CelebAResNet50(nn.Module):
         
         x = self.fc2(x)
         
+        return intermediate_output, x
+    
+class Cifar100VGG16(nn.Module):
+    def __init__(self, n_classes=100):
+        super().__init__()
+        vgg = vgg16(pretrained=True)
+        self.feature_extractor = nn.Sequential(
+            vgg.features,
+            nn.AdaptiveAvgPool2d((1, 1)) 
+        )
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(512, 512)
+        self.fc2 = nn.Linear(512, n_classes)
+        self.relu = nn.ReLU()
+        self.last_layer = self.fc2
+
+    def forward(self, x):
+        x = self.feature_extractor(x)
+        x = self.flatten(x)
+        x = self.relu(self.fc1(x))
+        intermediate_output = x
+        x = self.fc2(x)
         return intermediate_output, x
