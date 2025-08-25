538ff7aea2f6752e7aa9b156c7c9229ee54aee74
Claudio Savelli <claudiosav2000@gmail.com>
2025-08-26 01:42:26 +0200

    fixed srl for multilabel datasets

--- erasure/unlearners/SuccessiveRandomLabels.py
+++ erasure/unlearners/SuccessiveRandomLabels.py
@@ -20,11 +20,14 @@ class SuccessiveRandomLabels(TorchUnlearner):
         self.ref_data_retain = self.local.config['parameters']['ref_data_retain']  
         self.ref_data_forget = self.local.config['parameters']['ref_data_forget'] 
 
+        self.task = self.local.config['parameters']['task']
+
         self.predictor.optimizer = get_instance_kvargs(self.local_config['parameters']['optimizer']['class'],
                                       {'params':self.predictor.model.parameters(), **self.local_config['parameters']['optimizer']['parameters']})
 
         self.retain_set, _ = self.dataset.get_loader_for(self.ref_data_retain)
         self.forget_set, _ = self.dataset.get_loader_for(self.ref_data_forget)
+        
         self.n_classes = self.dataset.n_classes
 
         #unlearning_data = UnLearningData(forget_set=forget_set, retain_set=retain_set, n_classes=self.dataset.n_classes)
@@ -44,12 +47,19 @@ class SuccessiveRandomLabels(TorchUnlearner):
             for X, y in self.forget_set:
                 X, y = X.to(self.device), y.to(self.device)
 
-                random_labels = []
-                for label in y:
-                    random_label = np.random.choice([c for c in range(self.n_classes) if c != label.item()])
-                    random_labels.append(random_label)
-                labels = torch.tensor(random_labels, device=self.device)
-                
+                if self.task == 'auto':
+                    random_labels = []
+                    for label in y:
+                        random_label = np.random.choice([c for c in range(self.n_classes) if c != label.item()])
+                        random_labels.append(random_label)
+                    labels = torch.tensor(random_labels, device=self.device)
+                elif self.task == 'multilabel':
+                    # swap all labels
+                    labels = 1 - y
+                    labels = torch.abs(labels)
+                    labels = labels.to(self.device)
+
+
                 self.predictor.optimizer.zero_grad() 
 
                 _, output = self.predictor.model(X.to(self.device))
@@ -89,3 +99,4 @@ class SuccessiveRandomLabels(TorchUnlearner):
         self.local.config['parameters']['ref_data_retain'] = self.local.config['parameters'].get("ref_data_retain", 'retain')  # Default reference data is retain
         self.local.config['parameters']['ref_data_forget'] = self.local.config['parameters'].get("ref_data_forget", 'forget')  # Default reference data is forget
         self.local.config['parameters']['optimizer'] = self.local.config['parameters'].get("optimizer", {'class':'torch.optim.Adam', 'parameters':{}})  # Default optimizer is Adam
+        self.local.config['parameters']['task'] = self.local.config['parameters'].get("task", 'auto')  # Default task is auto (single-label classification)
